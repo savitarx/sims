12cea04d70a9575abe58a10c78053e698094c8cd
vik-ram-an-s <vikramansermakani@gmail.com>
2026-07-29 10:12:18 +0530

    Exception and Security Config

--- backend/src/main/java/com/invisos/sims/common/config/SecurityConfig.java
+++ backend/src/main/java/com/invisos/sims/common/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/teachers/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
